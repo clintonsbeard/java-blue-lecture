5c34b3dc11428870c2d2488031e95f4d64ebaf0c
Brian Lauvray <lauvray.1@osu.edu>
2019-01-14 22:30:07 -0500

    Added Variables and Data Types

---
+++ m1-02_Variables_Data_Types-lecture/dotnet/variables-and-datatypes-lecture/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace variables_and_datatypes_lecture
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
