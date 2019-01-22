a3f287f7522a59186570b12c5f61e30b5eaae5bf
Brian <brian@techelevator.com>
2019-01-22 10:24:33 -0500

    examples

--- m1-07_Collections_Part_1-lecture/src/main/java/com/techelevator/instructor/ClassExamplesFinal.java
+++ m1-07_Collections_Part_1-lecture/src/main/java/com/techelevator/instructor/ClassExamplesFinal.java
@@ -1,10 +1,20 @@
 package com.techelevator.instructor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ClassExamplesFinal {
 
 	public static void main(String[] args) {
+		
+		// without import
+		//java.time.LocalDateTime currentDateTime = java.time.LocalDateTime.now();
+		//System.out.println(currentDateTime.format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 
-
+		// With import
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+		
 	}
 
 }
