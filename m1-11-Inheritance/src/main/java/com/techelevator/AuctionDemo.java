78524ff8819dbca8af539460571f9abcf34b9d55
Clinton <clintonjbeard@gmail.com>
2019-01-29 09:02:33 -0500

    Committing to pull Tuesday lecture

---
+++ m1-11-Inheritance/src/main/java/com/techelevator/AuctionDemo.java
@@ -0,0 +1,45 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AuctionDemo {
+
+		public static void main(String[] args) {
+			
+			Scanner in = new Scanner(System.in);
+			
+//			Auction auction = new Auction("Record");
+//			Auction auction = new BuyoutAuction("Record", 10);
+//			Auction auction = new ReserveAuction("Taco", 3);
+			
+//			if (auction instanceof BuyoutAuction) {
+//				System.out.println("Auction is a Buyout Auction");
+//			}
+//			if (auction instanceof Auction) {
+//				System.out.println("Auction is an Auction");
+//			}
+			
+			List<Auction> myAuctions = new ArrayList<Auction>();
+			myAuctions.add(new Auction("Record"));
+			myAuctions.add(new BuyoutAuction("Book", 10));
+			myAuctions.add(new ReserveAuction("Taco", 10));
+			
+			for (Auction auction : myAuctions) {
+			System.out.println("Buy my " + auction.getItemForSale());
+			
+				System.out.print("Enter your name >>>");
+				String bidder = in.nextLine();
+				
+				System.out.print("Place a bid >>>");
+				String amount = in.nextLine();
+				
+				int bid = Integer.parseInt(amount);
+				
+				boolean isWinning = auction.placeBid(new Bid(bidder, bid));
+				
+				System.out.println(auction.getHighBid().getBidder() + " is winning with a bid of " + auction.getHighBid().getBidAmount());
+			}
+		}
+}
