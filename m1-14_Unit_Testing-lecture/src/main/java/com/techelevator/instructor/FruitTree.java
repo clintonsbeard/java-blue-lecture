1d4c3fc2aab05834dfefb42fec528dd1f311845f
Brian Lauvray <lauvray.1@osu.edu>
2019-01-30 18:57:03 -0500

    Added Unit Testing

---
+++ m1-14_Unit_Testing-lecture/src/main/java/com/techelevator/instructor/FruitTree.java
@@ -0,0 +1,31 @@
+package com.techelevator.instructor;
+
+public class FruitTree {
+
+	private String typeOfFruit;
+    private int piecesOfFruitLeft;
+
+    public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
+        this.piecesOfFruitLeft = startingPiecesOfFruit;
+        this.typeOfFruit = typeOfFruit;
+    }
+
+    public String getTypeOfFruit() {
+        return typeOfFruit;
+    }
+    
+    public int getPiecesOfFruitLeft() {
+        return piecesOfFruitLeft;
+    }
+
+    public boolean pickFruit(int numberOfPiecesToRemove) {
+        if (piecesOfFruitLeft-numberOfPiecesToRemove >= 0) {
+            piecesOfFruitLeft -= numberOfPiecesToRemove;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
