3364177144cf8ecf57b20567a7e3c421da265bbc
Clinton <clintonjbeard@gmail.com>
2019-02-04 09:16:41 -0500

    Committing to pull Monday lecture

--- m1-15_TDD-lecture/src/main/java/com/techelevator/KataTriangle.java
+++ m1-15_TDD-lecture/src/main/java/com/techelevator/KataTriangle.java
@@ -10,6 +10,74 @@ package com.techelevator;
  * 
  * If the lengths of the sides cannot correspond to a real triangle, then return "invalid"
  */
-public class KataTriangle {
 
+/* Given an input of 3 integers, which are lengths of the sides of a triangle
+ * Classifies the triangle as scalene, isosceles, equilateral, invalid
+ * Scalene - no sides equal
+ * Isosceles - two sides equal
+ * Equilateral - all sides equal
+ * Invalid - not a triangle  (sum of any two sides must be greater than the third)
+ */
+
+public class KataTriangle {
+	
+	public String classifyTriangle(int length1, int length2, int length3) {
+	
+		if (!isTriangle(length1, length2, length3)) {
+			return "Invalid";
+		}
+		if (isScalene(length1, length2, length3)) {
+			return "Scalene";
+		}
+		if (isEquilateral(length1, length2, length3)) {
+			return "Equilateral";
+		}
+		if (isIsosceles(length1, length2, length3)) {
+			return "Isosceles";
+		}
+		return null;
+	}
+	
+	public boolean isTriangle(int length1, int length2, int length3) {
+		
+		if (length1 < 1 || length2 < 1 || length3 < 1) {
+			return false;
+		}
+		if (length1 + length2 < length3 ||
+			length1 + length3 < length2 ||
+			length2 + length3 < length1) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean isScalene(int length1, int length2, int length3) {
+		
+		if (length1 != length2 &&
+			length1 != length3 &&
+			length2 != length3) {
+			return true;
+			}
+		return false;
+	}
+	
+	public boolean isEquilateral(int length1, int length2, int length3) {
+		
+		if (length1 == length2 &&
+			length1 == length3) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isIsosceles(int length1, int length2, int length3) {
+		
+		if (length1 == length2 ||
+			length1 == length3 ||
+			length2 == length3) {
+			return true;
+			}
+		return false;
+	}
+	
 }
--- m1-15_TDD-lecture/src/test/java/com/techelevator/KataTriangleTest.java
+++ m1-15_TDD-lecture/src/test/java/com/techelevator/KataTriangleTest.java
@@ -4,4 +4,70 @@ import org.junit.*;
 
 public class KataTriangleTest {
 
+	private KataTriangle target;
+	
+	@Before
+	public void setup() {
+		target = new KataTriangle();
+	}
+	
+	@Test
+	public void invalid_when_sum_of_first_and_second_sides_are_less_than_third() {
+		String type = target.classifyTriangle(1, 1, 3);
+		Assert.assertEquals("Invalid", type);
+	}
+	
+	@Test
+	public void invalid_when_sum_of_first_and_third_sides_are_less_than_second() {
+		String type = target.classifyTriangle(1, 3, 1);
+		Assert.assertEquals("Invalid", type);
+	}
+	
+	@Test
+	public void invalid_when_sum_of_second_and_third_sides_are_less_than_first() {
+		String type = target.classifyTriangle(3, 1, 1);
+		Assert.assertEquals("Invalid", type);
+	}
+	
+	@Test
+	public void invalid_when_one_or_more_sides_is_negative() {
+		String type = target.classifyTriangle(-1, -2, -3);
+		Assert.assertEquals("Invalid", type);
+	}
+	
+	@Test
+	public void invalid_when_one_or_more_sides_is_zero() {
+		String type = target.classifyTriangle(0, 1, 1);
+		Assert.assertEquals("Invalid", type);
+	}
+	
+	@Test
+	public void scalene_when_all_sides_are_different() {
+		String type = target.classifyTriangle(2, 3, 4);
+		Assert.assertEquals("Scalene", type);
+	}
+	
+	@Test
+	public void equilateral_when_all_sides_are_the_same() {
+		String type = target.classifyTriangle(3, 3, 3);
+		Assert.assertEquals("Equilateral", type);
+	}
+	
+	@Test
+	public void isosceles_when_first_and_second_sides_are_equal_() {
+		String type = target.classifyTriangle(2, 2, 3);
+		Assert.assertEquals("Isosceles", type);
+	}
+	
+	@Test
+	public void isosceles_when_first_and_third_sides_are_equal() {
+		String type = target.classifyTriangle(2, 3, 2);
+		Assert.assertEquals("Isosceles", type);
+	}
+	
+	@Test
+	public void isosceles_when_second_and_third_sides_are_equal() {
+		String type = target.classifyTriangle(3, 2, 2);
+		Assert.assertEquals("Isosceles", type);
+	}
 }
