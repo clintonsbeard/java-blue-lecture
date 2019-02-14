890ce694cbf8768afe9441c5203491bff0b6250c
Clinton <clintonjbeard@gmail.com>
2019-02-14 09:35:06 -0500

    Committing to pull Thursday lecture

--- m2-03_Joins-lecture/join-examples.sql
+++ m2-03_Joins-lecture/join-examples.sql
@@ -1,54 +1,41 @@
--- These queries require the JOINS database
+-- These queries require the JOINS database.
 
---INNER JOIN  (DEFAULT IF JUST USE JOIN)
+-- INNER JOIN (DEFAULT IF JUST USE JOIN)
+
 SELECT one.number AS one_number, one.description as one_description, two.number as two_number, two.description as two_description
-FROM one 
--- Complte the Inner Join Here
+FROM one JOIN two ON one.number = two.number;
 
+-- LEFT JOIN (Left Outer Join)
 
---LEFT JOIN  (Left Outer Join)
 SELECT one.number AS one_number, one.description as one_description, two.number as two_number, two.description as two_description
-FROM one
--- Complte the Left Join Here
+FROM one LEFT JOIN two ON one.number = two.number;
 
+-- RIGHT JOIN (Right Outer Join)
 
---RIGHT JOIN  (Right Outer Join)
 SELECT one.number AS one_number, one.description as one_description, two.number as two_number, two.description as two_description
-FROM one
--- Complte the Right Join Here
+FROM one RIGHT JOIN two ON one.number = two.number;
 
+-- FULL OUTER JOIN
 
---FULL OUTER JOIN
 SELECT one.number AS one_number, one.description as one_description, two.number as two_number, two.description as two_description
-FROM one
--- Complte the Full Outer Join Here
+FROM one FULL OUTER JOIN two ON one.number = two.number;
 
+-- Everything from ONLY the left table.
 
---Everything from ONLY the left table
 SELECT one.number AS one_number, one.description as one_description, two.number as two_number, two.description as two_description
-FROM one
--- Complte the Join Here
+FROM one LEFT JOIN two ON one.number = two.number WHERE two.number IS NULL;
 
+-- Everything from ONLY the right table.
 
---Everything from ONLY the right table
 SELECT one.number AS one_number, one.description as one_description, two.number as two_number, two.description as two_description
-FROM one
--- Complte the Join Here
+FROM one RIGHT JOIN two ON one.number = two.number WHERE one.number IS NULL;
 
+-- Everything in ONLY the Left or Right table, but not both.
 
---Everything in ONLY the Left or Right table, but not both
 SELECT one.number AS one_number, one.description as one_description, two.number as two_number, two.description as two_description
-FROM one
--- Complte the Join Here
+FROM one FULL OUTER JOIN two ON one.number = two.number WHERE one.number IS NULL OR two.number IS NULL;
 
+-- UNION
+-- Create a Union between the two tables that shows all values from both in two columns.
 
---UNION
--- Create a Union between the two tables that shows all values from both in two columns
-
-
-
-
-
-
-
-
+SELECT number, description FROM one UNION SELECT number, description FROM two ORDER BY number;
--- m2-03_Joins-lecture/lecture.sql
+++ m2-03_Joins-lecture/lecture.sql
@@ -2,21 +2,67 @@
 
 -- Let's find out who made payment 16666:
 
+SELECT * FROM payment WHERE payment_id = 16666;
+
 -- Ok, that gives us a customer_id, but not the name. We can use the customer_id to get the name FROM the customer table
+
+SELECT * FROM payment JOIN customer ON payment.customer_id = customer.customer_id WHERE payment_id = 16666;
 
 -- We can see that the * pulls back everything from both tables. We just want everything from payment and then the first and last name of the customer:
 
+SELECT payment.*, customer.first_name, customer.last_name FROM payment JOIN customer ON payment.customer_id = customer.customer_id WHERE payment_id = 16666;
+
 -- But when did they return the rental? Where would that data come from? From the rental table, so let’s join that.
+
+SELECT payment.*, customer.first_name, customer.last_name, rental.return_date 
+FROM payment 
+JOIN customer ON payment.customer_id = customer.customer_id 
+JOIN rental ON rental.rental_id = payment.rental_id
+WHERE payment_id = 16666;
 
 -- What did they rent? Film id can be gotten through inventory.
 
+SELECT payment.*, customer.first_name, customer.last_name, rental.return_date, film.title 
+FROM payment 
+JOIN customer ON payment.customer_id = customer.customer_id 
+JOIN rental ON rental.rental_id = payment.rental_id
+JOIN inventory ON inventory.inventory_id = rental.inventory_id
+JOIN film ON film.film_id = inventory.film_id
+WHERE payment_id = 16666;
+
 -- What if we wanted to know who acted in that film?
+
+SELECT film.title, (actor.first_name || ' ' || actor.last_name) AS actor
+FROM film
+JOIN film_actor ON film_actor.film_id = film.film_id
+JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE film.film_id = 948;
 
 -- What if we wanted a list of all the films and their categories ordered by film title
 
+SELECT film.title, category.name 
+FROM film 
+JOIN film_category ON film_category.film_id = film.film_id
+JOIN category ON category.category_id = film_category.category_id
+ORDER BY film.title;
+
 -- Show all the 'Comedy' films ordered by film title
 
+SELECT f.title AS comedy_titles
+FROM film f
+JOIN film_category fc ON fc.film_id = f.film_id
+JOIN category c ON c.category_id = fc.category_id
+WHERE c.name = 'Comedy'
+ORDER BY f.title;
+
 -- Finally, let's count the number of films under each category
+
+SELECT c.name, COUNT(f.title) AS number_of_movies
+FROM film f
+JOIN film_category fc ON fc.film_id = f.film_id
+JOIN category c ON c.category_id = fc.category_id
+GROUP BY c.name
+ORDER BY c.name;
 
 -- ********* LEFT JOIN ***********
 
@@ -26,17 +72,38 @@
 
 -- Let's display a list of all countries and their capitals, if they have some.
 
+SELECT c.name, ci.name
+FROM country c
+JOIN city ci ON c.capital = ci.id;
+
 -- Only 232 rows
 -- But we’re missing entries:
 
+SELECT COUNT (*) FROM country;
+
 -- There are 239 countries. So how do we show them all even if they don’t have a capital?
 -- That’s because if the rows don’t exist in both tables, we won’t show any information for it. If we want to show data FROM the left side table everytime, we can use a different join:
+
+SELECT c.name, ci.name
+FROM country c
+LEFT JOIN city ci ON c.capital = ci.id;
 
 -- *********** UNION *************
 
 -- Back to the "dvdstore" database...
 
 -- Gathers a list of all first names used by actors and customers
+
 -- By default removes duplicates
 
+SELECT first_name FROM actor
+UNION
+SELECT first_name FROM customer
+ORDER BY first_name;
+
 -- Gather the list, but this time note the source table with 'A' for actor and 'C' for customer
+
+SELECT first_name, 'A' AS source FROM actor
+UNION
+SELECT first_name, 'C' AS source FROM customer
+ORDER BY first_name;
