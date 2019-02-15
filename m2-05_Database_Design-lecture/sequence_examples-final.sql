6f6ba2505a13d7aa615589a829d42d3630061881
Brian <brian@techelevator.com>
2019-02-15 12:50:53 -0500

    Added examples from lecture

---
+++ m2-05_Database_Design-lecture/sequence_examples-final.sql
@@ -0,0 +1,32 @@
+CREATE TABLE ourtable (
+        id serial primary key,
+        name varchar(35)
+);
+
+
+SELECT * FROM ourtable;
+
+-- Can insert with a sequence by just omitting the column
+INSERT INTO ourtable (name) VALUES ('Joe');
+
+-- Can insert using the DEFAULT keyword
+INSERT INTO ourtable (id, name) VALUES (DEFAULT, 'John');
+
+-- Can get the next value in our sequence
+SELECT nextval('ourtable_id_seq');
+
+-- Can insert the id manually
+INSERT INTO ourtable (id, name) VALUES (7, 'Sue');
+
+CREATE SEQUENCE our_custom_sequence START 101;
+
+SELECT nextval('our_custom_sequence');
+
+ALTER SEQUENCE our_custom_sequence RESTART 1000 INCREMENT BY 2;
+
+START TRANSACTION;
+INSERT INTO ourtable (id, name) VALUES (DEFAULT, 'Bill');
+ROLLBACK;
+
+DROP SEQUENCE our_custom_sequence;
+DROP TABLE ourtable;
